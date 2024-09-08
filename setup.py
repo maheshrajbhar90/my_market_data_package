e677ee7466f733b3899a8e5fc48bdf7979b47cb1
maheshrajbhar90 <127003692+maheshrajbhar90@users.noreply.github.com>
2024-09-08 20:04:00 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_project',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[],  # List dependencies here
+    description='A brief description of my project',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Your Name',
+    author_email='your.email@example.com',
+    url='https://github.com/yourusername/my_project',  # Replace with your project URL
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
  
